00c6f93020ff5346a5e775c8a25e03c95f16a0b2
vaanessamota <vanessaoliveira2706@gmail.com>
2023-07-18 08:32:41 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Vanessa Oliveira on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
