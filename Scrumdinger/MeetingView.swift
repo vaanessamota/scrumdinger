a52d78be88a674fccec68f4330f3bd0c93381119
vaanessamota <vanessaoliveira2706@gmail.com>
2023-07-18 10:39:34 -0300

    using stacks to arrange views

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Vanessa Oliveira on 18/07/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 15, total: 30)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle().stroke(lineWidth: 25)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }.padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
